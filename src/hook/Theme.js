a8f5303101ff0132bc455b7f06ee0c2521411b06
David Delahaye <DSMI@blackpool.ac.uk>
2023-11-01 22:03:28 +0000

    add themes

---
+++ src/hook/Theme.js
@@ -0,0 +1,17 @@
+import { useEffect, useState } from "react";
+
+export default function useTheme() {
+    const [theme, setTheme] = useState(localStorage.theme);
+  
+    useEffect(() => {
+      const oldTheme = theme === 'dark' ? 'light' : 'dark';
+      const root = window.document.documentElement;
+      console.log(theme);
+      root.classList.remove(oldTheme);
+      root.classList.add(theme);
+  
+      localStorage.setItem('theme', theme);
+    }, [theme]);
+  
+    return [theme, setTheme];
+  }
