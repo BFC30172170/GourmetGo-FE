a9c17aceeacf40ba87f1b2c0eec5ca24de67fb35
David Delahaye <dev@david-delahaye.co.uk>
2023-11-16 16:31:03 +0000

    code cleanup, minor style changes

--- src/Layout.js
+++ src/Layout.js
@@ -15,7 +15,7 @@ function Layout() {
     const location = useLocation();
     const basket = useBasket();
     const [userDropdown, setUserDropdown] = useState(false);
-    const [cartDropdown, setCartDropdown] = useState();
+    const [cartDropdown, setCartDropdown] = useState(false);
     const cartRef = useRef();
     const userRef = useRef();
     const toggleTheme = (e) => {
@@ -26,21 +26,21 @@ function Layout() {
         }
     };
 
-    const handleOffClick = (e) =>{
-        if(cartRef && cartRef.current.contains(e.target)){
-        }else{
+    const handleOffClick = (e) => {
+        if (cartRef && cartRef.current.contains(e.target)) {
+        } else {
             setCartDropdown(false);
         }
-        if(userRef && userRef.current.contains(e.target)){
+        if (userRef && userRef.current.contains(e.target)) {
 
-        }else{
+        } else {
             setUserDropdown(false);
         }
 
 
     }
     return (
-        <div className="relative" onClick={(e) => {handleOffClick(e)}}>
+        <div className="relative" onClick={(e) => { handleOffClick(e) }}>
             <header className="absolute inset-x-0 top-0 z-50 bg-gray-100 dark:bg-gray-900 transition duration-300">
                 <nav className=" max-w-7xl mx-auto flex items-center justify-between p-6 lg:px-8" aria-label="Global">
                     <div className="flex lg:flex-1">
@@ -49,46 +49,53 @@ function Layout() {
                             {theme == 'light' ? <img className="h-8 w-auto" src={logolight} alt="" /> : <img className="h-8 w-auto" src={logodark} alt="" />}
                         </NavLink>
                     </div>
-                    {/* User */}
+
                     <div className="flex gap-6 text-gray-900 dark:text-gray-50 transition duration-300">
-                        <div ref={userRef} className="text-sm font-semibold leading-6 relative" onClick={(e) => setUserDropdown(true)}><UserIcon className="w-6 h-6" />
+                        {/* User Menu */}
+                        <div ref={userRef} className="text-sm font-semibold leading-6 relative" >
+                            <UserIcon className="w-6 h-6 hover:scale-105 hover:text-red-500 duration-300 cursor-pointer" onClick={(e) => setUserDropdown(!userDropdown)} />
                             <div class={`absolute ${userDropdown ? "block" : "hidden"}  p-2 bg-gray-50 dark:bg-gray-900 text-gray-700 dark:text-gray-300 right-0 z-10 mt-2 w-56 origin-top-right rounded-md shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none`} role="menu" aria-orientation="vertical" aria-labelledby="menu-button" tabindex="-1">
                                 <div class="py-1 flex flex-col" role="none">
-                                {auth.user === null ?
-                                    <>
-                                    <NavLink to="/login" className="block px-4 py-2 text-sm" role="menuitem" tabindex="-1" id="menu-item-0">Login</NavLink>
-                                    <NavLink to="/signup" className="block px-4 py-2 text-sm" role="menuitem" tabindex="-1" id="menu-item-1">Signup</NavLink>
-                                    </>
-                                    :
-                                    <>
-                                    <li onClick={(e)=>{auth.logout()}}className="block px-4 py-2 text-sm" role="menuitem" tabindex="-1" id="menu-item-1">Logout</li>
-                                    <NavLink to="/" className="block px-4 py-2 text-sm" role="menuitem" tabindex="-1" id="menu-item-2">{auth.username}'s Profile</NavLink>
-                                    </>
-                                }
+                                    {auth.user === null ?
+                                        <>
+                                            <NavLink onClick={(e) => setUserDropdown(false)} to="/login" className="block px-4 py-2 text-sm rounded-lg duration-300 dark:hover:bg-gray-800 hover:bg-gray-200" role="menuitem" tabindex="-1" id="menu-item-0">Login</NavLink>
+                                            <NavLink onClick={(e) => setUserDropdown(false)} to="/signup" className="block px-4 py-2 text-sm rounded-lg duration-300 dark:hover:bg-gray-800 hover:bg-gray-200" role="menuitem" tabindex="-1" id="menu-item-1">Signup</NavLink>
+                                        </>
+                                        :
+                                        <>
+                                            <li onClick={(e) => { setUserDropdown(false); auth.logout() }} className="block px-4 py-2 text-sm rounded-lg cursor-pointer duration-300 dark:hover:bg-gray-800 hover:bg-gray-200" role="menuitem" tabindex="-1" id="menu-item-1">Logout</li>
+                                            <NavLink onClick={(e) => setUserDropdown(false)} to="/profile" className="block px-4 py-2 text-sm rounded-lg duration-300 dark:hover:bg-gray-800 hover:bg-gray-200" role="menuitem" tabindex="-1" id="menu-item-2">{auth.username}'s Profile</NavLink>
+                                        </>
+                                    }
                                 </div>
                             </div>
                         </div>
 
-                        <div href="#" ref={cartRef} className="text-sm font-semibold leading-6 relative" onClick={(e) => setCartDropdown(true)}><ShoppingCartIcon className="w-6 h-6" />
-                            <div class={`absolute ${cartDropdown ? "block" : "hidden"} p-2 bg-gray-50 dark:bg-gray-900 text-gray-700 dark:text-gray-300 right-0 z-10 mt-2 w-56 origin-top-right rounded-md shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none`} role="menu" aria-orientation="vertical" aria-labelledby="menu-button" tabindex="-1">
-                                <div class="py-1" role="none">
-                                    {basket.items.map((item,i) => {
-                                        return(
+                        {/* Basket Menu */}
+                        <div ref={cartRef} className="text-sm font-semibold leading-6 relative" onClick={(e) => setCartDropdown(!cartDropdown)}>
+                            <ShoppingCartIcon className="w-6 h-6 hover:scale-105 hover:text-red-500 duration-300 cursor-pointer" />
+                            <div className='absolute -right-2 -top-3'>
+                                {basket?.items?.length}
+                                </div>
+                            <div className={`absolute ${cartDropdown ? "block" : "hidden"} p-2 bg-gray-50 dark:bg-gray-900 text-gray-700 dark:text-gray-300 right-0 z-10 mt-2 w-56 origin-top-right rounded-md shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none`} role="menu" aria-orientation="vertical" aria-labelledby="menu-button" tabindex="-1">
+                                <div className="py-1" role="none">
+                                    {basket.items.map((item, i) => {
+                                        return (
                                             <a href="#" class="block px-4 py-2 text-sm" role="menuitem" tabindex="-1" id="menu-item-0">{item.name} - £{item.price}</a>
                                         )
-                                    })}                       
+                                    })}
                                 </div>
                                 <div class="py-1" role="none">
-                                    <button href="#" className="px-4 py-2 text-sm bg-red-500 flex rounded-2xl mx-auto w-full text-center" role="menuitem" tabindex="-1" id="menu-item-2">Checkout</button>
+                                    <button href="#" onClick={(e) => setCartDropdown(false)} className="px-4 py-2 text-sm bg-red-500 text-gray-50 flex rounded-lg mx-auto w-full text-center" role="menuitem" tabindex="-1" id="menu-item-2">Checkout</button>
                                 </div>
                             </div>
                         </div>
-                        <a href="#" className="text-sm font-semibold leading-6">{theme == 'light' ? <SunIcon className="w-6 h-6" onClick={(e) => { toggleTheme() }} /> : <MoonIcon className="w-6 h-6" onClick={(e) => { toggleTheme() }} />}</a>
+                        <a href="#" className="text-sm font-semibold leading-6 hover:scale-105 hover:text-red-500 duration-300 cursor-pointer">{theme == 'light' ? <SunIcon className="w-6 h-6" onClick={(e) => { toggleTheme() }} /> : <MoonIcon className="w-6 h-6" onClick={(e) => { toggleTheme() }} />}</a>
                     </div>
                 </nav>
             </header>
 
-           <Outlet/>
+            <Outlet />
 
             <footer className='bg-gray-100 dark:bg-gray-900 transition duration-300 h-32 w-full'>
                 footer
--- src/page/RestaurantIndex.js
+++ src/page/RestaurantIndex.js
@@ -3,77 +3,58 @@ import { Link } from 'react-router-dom';
 import { PencilIcon } from '@heroicons/react/24/solid';
 
 import { useLocation } from 'lib/location';
+import { haversineDistance } from 'utils/distance';
 
 function RestaurantIndex() {
     const [restaurants, setRestaurants] = useState([]);
     const [categories, setCategories] = useState([]);
     const [filters, setFilters] = useState([]);
     const [search, setSearch] = useState('');
-    const [sort, setSort] = useState(null);
     const location = useLocation();
 
     const getRestaurants = async () => {
+        // Fetch restaurants
         const res = await fetch('https://654a0134e182221f8d524e9c.mockapi.io/Restaurants');
         const json = await res.json();
         let restaurants = json;
+        // Get only those with search term
         restaurants = restaurants.filter((restaurant) => restaurant.name.toLowerCase().indexOf(search.toLowerCase()) > -1)
+        // If there are filters, apply them
         if (filters.length > 0) {
             restaurants = restaurants.filter((restaurant) => filters.includes(restaurant.category))
         }
-        restaurants.sort((a, b) => { return a.name.localeCompare(b.name) });
+        //Sort by smallest distance ascending
+        restaurants.sort((a, b) => { return haversineDistance([a.longitude, a.latitude], [location.longitude, location.latitude]) - haversineDistance([b.longitude, b.latitude], [location.longitude, location.latitude]) });
         setRestaurants(restaurants);
     };
 
     useEffect(() => {
-        const timer = setTimeout(()=>{
-        getRestaurants();
-        return;
-        },350)
-        return ()=>clearTimeout(timer);
-    }, [filters, search, sort])
+        //When filter or sort changes, refetch Restaurants with this criteria
+        const timer = setTimeout(() => {
+            getRestaurants();
+            return;
+        }, 350)
+
+        return () => clearTimeout(timer);
+    }, [filters, search])
 
     useEffect(() => {
+        // When restaurants load, if there are not categories loaded yet, load each distinc tone
         if (categories.length < 1) {
             const cats = getDistinctCategories(restaurants);
             setCategories(cats);
         }
-
     }, [restaurants])
 
-    const haversineDistance = (coords1, coords2, isMiles) => {
-        function toRad(x) {
-            return x * Math.PI / 180;
-        }
-        var lon1 = coords1[0];
-        var lat1 = coords1[1];
-
-        var lon2 = coords2[0];
-        var lat2 = coords2[1];
-
-        var R = 6371; // km
-
-        var x1 = lat2 - lat1;
-        var dLat = toRad(x1);
-        var x2 = lon2 - lon1;
-        var dLon = toRad(x2)
-        var a = Math.sin(dLat / 2) * Math.sin(dLat / 2) +
-            Math.cos(toRad(lat1)) * Math.cos(toRad(lat2)) *
-            Math.sin(dLon / 2) * Math.sin(dLon / 2);
-        var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
-        var d = R * c;
-
-        if (isMiles) d /= 1.60934;
-
-        return d.toFixed(0);
-    }
-
     const getDistinctCategories = (objects) => {
-
+        //To get all distinct categories from restaurants
+        //Get all the distinct categories
         let unique_values = objects
             .map((item) => item.category)
             .filter(
                 (value, index, current_value) => current_value.indexOf(value) === index
             );
+        //Then go through all of these, and get the counts of products with these categories
         const cats = [];
         unique_values.forEach(cat => {
             let newCat = {};
@@ -81,30 +62,39 @@ function RestaurantIndex() {
             newCat.count = restaurants.filter((restaurant) => restaurant.category == cat).length;
             cats.push(newCat);
         });
-        cats.sort((a,b) => b.count - a.count)
+        //Then sort them descending by count
+        cats.sort((a, b) => b.count - a.count)
         return cats;
     }
 
     const handleFilter = async (e, i) => {
+        // When a filter is clicked, check if it is active
         if (filters.includes(e.target.value)) {
+            //If its active, inactivate it
             const newFilters = filters.slice();
             const index = newFilters.indexOf(e.target.value);
             newFilters.splice(index, 1);
             setFilters(newFilters);
         } else {
+            // Otherwise activate it
             const newFilters = filters.slice();
             newFilters.push(e.target.value);
             setFilters(newFilters)
         }
-        console.log(filters);
     }
 
     return (
         <div className="bg-gray-200 dark:bg-gray-800 text-gray-900 dark:text-gray-50 pt-32 min-h-screen">
             <main className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8 grid grid-cols-3 lg:grid-cols-4 gap-4">
                 <div className='hidden lg:block col-span-1 row-span-1'></div>
-                <div className='col-span-3'> <h1 className="text-4xl font-bold tracking-tight flex items-center">Restaurants near <span className='text-red-500 flex items-center gap-1 pl-4'><Link to="/" className='duration-300 hover:-rotate-12 hover:scale-110'><PencilIcon className='w-6 h-6'/></Link>{location.area}, {location.postcode}</span></h1></div>
+                {/* Title */}
+                <section className='col-span-3'> 
+                <h1 className="text-4xl font-bold tracking-tight flex items-center">
+                    Restaurants near <span className='text-red-500 flex items-center gap-1 pl-4'><Link to="/" className='duration-300 hover:-rotate-12 hover:scale-110'><PencilIcon className='w-6 h-6' /></Link>{location.area}, {location.postcode}</span>
+                    </h1>
+                </section>
 
+                {/* Category sidebar */}
                 <section aria-labelledby="products-heading" className="hidden lg:block col-span-1 row-span-4 rounded-2xl  ">
                     <form className=" bg-gray-100 dark:bg-gray-900 p-4 rounded-lg">
                         <h3 className="sr-only">Categories</h3>
@@ -132,27 +122,25 @@ function RestaurantIndex() {
                         </div>
                     </form>
                 </section>
-                <section className="flex items-baseline justify-between col-span-3">
-                    {/* Search*/}
-                    <input className='p-2 pl-6 rounded-2xl bg-gray-100 dark:bg-gray-900 w-full' placeholder='Search for a Restaurant' value={search} onChange={(e) => { setSearch(e.target.value) }} />
 
+                {/* Search */}
+                <section className="flex items-baseline justify-between col-span-3">
+                    <input className='p-2 pl-6 rounded-2xl bg-gray-100 dark:bg-gray-900 w-full' placeholder='Search for a Restaurant' value={search} onChange={(e) => { setSearch(e.target.value) }} />
                 </section>
 
-
-
+                {/* Product Grid */}
                 <section className='col-span-3'>
-                    {/* Products */}
                     <div className=" col-span-3 w-full p-4 max-w-7xl mx-auto transition duration-300">
                         <div className="w-full mx-auto grid grid-cols-2 lg:grid-cols-3 gap-4">
                             {restaurants.map((restaurant, i) => {
                                 return (
                                     <Link to={`/restaurants/${restaurant.id}`} className="group" key={i}>
                                         <div className="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-lg bg-gray-500 xl:aspect-h-8 xl:aspect-w-7">
-                                            <img src={restaurant.image} alt="Tall slender porcelain bottle with natural clay textured body and cork stopper." className="h-full w-full object-cover object-center group-hover:opacity-75" />
+                                            <img src={restaurant.image} alt={'food from ' + restaurant.name} className="h-full w-full object-cover object-center duration-300 group-hover:scale-105 group-hover:-rotate-1" />
                                         </div>
-                                        <div className="flex justify-between mt-4">
+                                        <div className="flex justify-between mt-4 font-medium group-hover:text-red-500 duration-300">
                                             <h3 className="text-sm">{restaurant.name}</h3>
-                                            <p className="text-sm font-medium text-right">{haversineDistance([restaurant.longitude, restaurant.latitude], [-3.020520, 53.809350])}km away</p>
+                                            <p className="text-sm text-right">{haversineDistance([restaurant.longitude, restaurant.latitude], [location.longitude, location.latitude])}km away</p>
                                         </div>
                                     </Link>
                                 )
