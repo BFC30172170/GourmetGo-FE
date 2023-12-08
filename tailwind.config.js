2979a8e443131e21b79eef290960550c9284909a
David Delahaye <dev@david-delahaye.co.uk>
2023-12-08 22:34:29 +0000

    add missing animation for logos

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,17 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        fire: {
+          '0%, 100%': { transform: 'rotate(-3deg)',opacity:'70%' },
+          '50%': { transform: 'rotate(3deg)',opacity:'100%' },
+        }
+      },
+      animation: {
+        fire: 'fire 1s ease-in-out infinite',
+      }
+    },
   },
   plugins: [],
   darkMode: 'class'
